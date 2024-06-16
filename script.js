08bd010e8bc17ecd3319c471b3308abc3a7d204b
UdeeshaRukshan <IT21807480@my.sliit.lk>
2024-06-16 20:43:52 +0530

    backend is done

---
+++ script.js
@@ -0,0 +1,30 @@
+const socket=io('http://localhost:3000');
+const messageForm=document.getElementById('send-container');
+const messageInput=document.getElementById('message-input')
+
+const name=prompt('What is your name?');
+appendMessage('You joined')
+socket.emit('new-user', name)
+
+
+socket.on('chat-message',data=>{
+
+    appendMessage(`${data.name}:${data.message}`)
+}
+)
+
+socket.on('user-connected',name=>{
+    appendMessage(`${name} connected`)
+})
+messageForm.addEventListener('submit',e=>{
+    e.preventDefault()
+    const message= messageInput.value
+    socket.emit('send-chat-message',message)
+    messageInput.value=''
+})
+
+function appendMessage(message){
+    const messageElement=document.createElement('div')
+    messageElement.innerText=message
+    messageContainer.append(messageElement)
+}
